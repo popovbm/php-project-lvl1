47166755bc5784752513afd655d6648cae127bfd
Ivan Popov <popovbm@gmail.com>
2022-05-02 17:37:36 +0300

    fix src/Cli

--- src/Cli.php
+++ src/Cli.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Brain\Games\Cli;
+    namespace Brain\Games\Cli;
 
-use function cli\line;
-use function cli\prompt;
+    use function cli\line;
+    use function cli\prompt;
 
 function greeting()
 {
-    line('Welcome to the Brain Games!');
+    line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
